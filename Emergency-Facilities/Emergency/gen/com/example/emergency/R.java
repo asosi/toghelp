0b2e6e9ec4e08c9c384d926f0a1920136e031a2f
BFranz <franzb475@gmail.com>
2014-04-02 16:34:25 +0200

    Due

--- Emergency-Facilities/Emergency/gen/com/example/emergency/R.java
+++ Emergency-Facilities/Emergency/gen/com/example/emergency/R.java
@@ -24,18 +24,24 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int askhelp=0x7f080000;
+        public static final int givehelp=0x7f080001;
+        public static final int situation=0x7f080002;
     }
     public static final class layout {
         public static final int activity_mappa=0x7f030000;
+        public static final int activity_start_screen=0x7f030001;
     }
     public static final class menu {
         public static final int situation=0x7f070000;
+        public static final int start_screen=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_start_screen=0x7f050003;
     }
     public static final class style {
         /** 
---
+++ Emergency-Facilities/Emergency/src/com/example/emergency/StartScreen.java
@@ -0,0 +1,22 @@
+package com.example.emergency;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class StartScreen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_start_screen);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.start_screen, menu);
+		return true;
+	}
+
+}
